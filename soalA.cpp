8da72f5e46d8942a671742ab921393bc263d9ae3
bimmbim <bimabaraja76@gmail.com>
2025-05-17 16:37:22 +0700

    Menambahkan Kelas Staff dengan Friend Class

--- soalA.cpp
+++ soalA.cpp
@@ -44,5 +44,32 @@ public:
     friend void lihatGajiDosen(Dosen* d);
 };
 
+// === Class Staff ===
+class Staff {
+private:
+    string nama;
+    string IDStaff;
+    float gaji;
 
+public:
+    Staff(string nama, string IDStaff, float gaji) : nama(nama), IDStaff(IDStaff), gaji(gaji) {}
+
+    // Method untuk mengubah pangkat dosen menggunakan pointer
+    void ubahPangkat(Dosen* d, string pangkatBaru) {
+        d->pangkat = pangkatBaru;
+    }
+
+    // Friend function untuk melihat gaji staff
+    friend void lihatGajiStaff(Staff* s);
+};
+
+// Friend Function untuk melihat gaji dosen
+void lihatGajiDosen(Dosen* d) {
+    cout << "Gaji Dosen: " << d->gaji << endl;
+}
+
+// Friend Function untuk melihat gaji staff
+void lihatGajiStaff(Staff* s) {
+    cout << "Gaji Staff: " << s->gaji << endl;
+}
 

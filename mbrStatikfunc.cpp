4cae771d4f9ba4ee9ea2c2571d200e1ab8520399
bimmbim <bimabaraja76@gmail.com>
2025-05-17 14:42:08 +0700

    commit19

---
+++ mbrStatikfunc.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+    static int nim;
+    int id;
+    string nama;
+
+public:
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim); // Definisi Function
+    static int getNim(); // Definisi Function
+
+    mahasiswa(string pNama) : nama(pNama) { setID(); }
+};
+
+
+
+
+
+
+
+
+
+

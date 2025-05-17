67ef8010b2584b3dc3ccdf1edaa29c26901caaba
bimmbim <bimabaraja76@gmail.com>
2025-05-17 14:35:09 +0700

    commit12

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -7,7 +7,12 @@ private:
     int nim;
     string nama;
 
-
+public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
 };
 
 

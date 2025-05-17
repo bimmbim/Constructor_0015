07767f0f34dc3528166706bcd3ef595a5a3bf830
bimmbim <bimabaraja76@gmail.com>
2025-05-17 14:28:48 +0700

    commit2

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -5,7 +5,17 @@ class buku
 {
     string judul;
 
+public:
+    buku setJudul(string judul)
+    {
+        this->judul = judul;
+        return *this; // chain function
+    }
 
+    string getJudul()
+    {
+        return this->judul;
+    }
 };
 
 
